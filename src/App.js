9866200fef6de1c98bfeccfbef49a43b4551557b
kohshow <kohshow@kohshownoMacBook-Air.local>
2020-03-20 00:58:52 +0900

    highcharts graph

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 import './App.css';
 import APIkey from './APIkey';
 
@@ -29,12 +31,38 @@ class App extends Component {
     selected_copy[index] = !selected_copy[index];
 
     if (!this.state.selected[index]) {
-      this.setState({
-        //selected: selected_copy
+      // チェックされていなかった場合: データ取得
+      // RESAS API 人口構成: https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
+      fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=-&prefCode=${index + 1}`,
+      { headers: { 'X-API-KEY': APIkey} }
+      )
+      .then(response => response.json())
+      .then(res => {
+        let tmp = [];
+        // data[0]はlabel="総人口"に相当する
+        Object.keys(res.result.data[0].data).forEach(i => {
+          tmp.push(res.result.data[0].data[i].value);
+        });
+        const res_series = {
+          name: this.state.prefectures[index].prefName,
+          data: tmp
+        };
+        this.setState({
+          selected: selected_copy,
+          series: [...this.state.series, res_series]
+        });
       });
     } else {
+      const series_copy = this.state.series.slice();
+      // チェック済みの場合: seriesから除外
+      for (let i = 0; i < series_copy.length; i++) {
+        if (series_copy[i].name == this.state.prefectures[index].prefName) {
+          series_copy.splice(i, 1);
+        }
+      }
       this.setState({
-        //selected: selected_copy
+        selected: selected_copy,
+        series: series_copy
       });
     }
   }
@@ -54,10 +82,44 @@ class App extends Component {
 
   render() {
     const data = this.state.prefectures;
+    const options = {
+      title: {
+        text: ''
+      },
+      yAxis: {
+        title: {
+          text: '人口数'
+        }
+      },
+      xAxis: {
+        title: {
+          text: '年度'
+        },
+        accessibility: {
+          rangeDescription: 'Range: 2010 to 2017'
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top'
+      },
+      plotOptions: {
+        series: {
+          label: {
+            connectorAllowed: false
+          },
+          pointInterval: 3,
+          pointStart: 1970
+        }
+      },
+      series: this.state.series
+    };
     return (
       <div>
         <h1>都道府県</h1>
         {Object.keys(data).map(i => this.renderItem(data[i]))}
+        <HighchartsReact highcharts={Highcharts} options={options} />
       </div>
     );
   }
