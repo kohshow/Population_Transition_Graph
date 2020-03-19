ec6327233cd9a2520439dd40fd8872fe6099ecc2
kohshow <kohshow@kohshownoMacBook-Air.local>
2020-03-20 04:21:18 +0900

    Updates

---
+++ precache-manifest.01cbf6fbb3e9ac589424b3ce3fc44202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d98b43a45049e84e5650ea702c3efa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd978bc47b18482124d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "2a62c9da7d664339ca19",
+    "url": "/static/js/2.6dad48dc.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.6dad48dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cd978bc47b18482124d",
+    "url": "/static/js/main.b59392f4.chunk.js"
+  },
+  {
+    "revision": "1f72de0c79f63bf588c3",
+    "url": "/static/js/runtime-main.2f742e68.js"
+  }
+]);
